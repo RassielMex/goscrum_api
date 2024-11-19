2428b8cd08032f8367621816daf8124941428b61
RassielMex <rhcp94.rm@gmail.com>
2024-11-19 12:19:56 -0600

    Feat: Vercel config for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { UsersModule } from './users/users.module';
 import { TeamsModule } from './teams/teams.module';
 import { TasksModule } from './tasks/tasks.module';
 import { PrismaService } from './prisma/prisma';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UsersModule, TeamsModule, TasksModule],
-  controllers: [],
-  providers: [PrismaService],
+  controllers: [AppController],
+  providers: [PrismaService, AppService],
 })
 export class AppModule {}
